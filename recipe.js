0b6c3d760a88f65618c1160ba785a62b50a51b9c
katherinethai <kthai1618@gmail.com>
2016-05-28 12:49:59 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ingredientName, amount) {
+  recipe[ingredientName] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredientName) {
+  delete recipe[ingredientName];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, amount) {
+  recipe[ingredientName] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var ingredient in recipe) {
+    console.log("this recipe calls for " + recipe[ingredient] + " of " + ingredient);
+  }
+}
